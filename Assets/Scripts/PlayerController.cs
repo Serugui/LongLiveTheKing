3aba9286d28c8b7076c32e962aac2cc74b317122
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2025-05-30 17:18:35 +0100

    Add Skybox & Clamp Movement
    For more immersive experience a Skybox from LaFinca free assets was added, and the movement was clamped so player cannot go out of boundary

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f; // Speed at which the player moves
+    [SerializeField] float xClamp = 3f;
+    [SerializeField] float zClamp = 3f;
     Vector2 movement;
     Rigidbody rigidBody;
 
@@ -28,6 +30,10 @@ void Awake()
         Vector3 currentPosition = rigidBody.position;
         Vector3 moveDirection = new Vector3(movement.x, 0, movement.y);
         Vector3 newPosition = currentPosition + moveDirection *(moveSpeed * Time.fixedDeltaTime);
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -xClamp, xClamp);
+        newPosition.z = Mathf.Clamp(newPosition.z, -zClamp, zClamp);
+
         rigidBody.MovePosition(newPosition);
     }
 }
