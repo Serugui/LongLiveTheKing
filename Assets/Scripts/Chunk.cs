e8bdf2670888e61ac241d50e3b998c87ea46b73e
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2025-05-30 21:39:34 +0100

    Add coin spawn logic
    More than one coin can be instantiated per chunk

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -4,27 +4,69 @@ using System.Collections.Generic;
 public class Chunk : MonoBehaviour
 {
     [SerializeField] GameObject fencePrefab;
+    [SerializeField] GameObject applePrefab;
+    [SerializeField] GameObject coinPrefab;
+
     [SerializeField] float[] lanes = {-2.5f, 0f, 2.5f};
+    [SerializeField] float appleSpawnChance = .3f;
+    [SerializeField] float coinSpawnChance = .5f;
+    [SerializeField] float coinSeparationLength = 2f; // Distance between coins
+
+    List<int> availableLanes = new List<int>{0, 1, 2};
 
     void Start()
     {
-        SpawnFence();
+        SpawnFences();
+        SpawnApple();
+        SpawnCoins();
     }
 
-    void SpawnFence()
+    void SpawnFences()
     {
-        List<int> availableLanes = new List<int>{0, 1, 2};
-        int fencesToSpawn = Random.Range(0, 3);
+        int fencesToSpawn = Random.Range(0, lanes.Length);
 
         for (int i = 0; i < fencesToSpawn; i++)
         {
-            int randomLaneIndex = Random.Range(0, availableLanes.Count);
-            int selectedLane = availableLanes[randomLaneIndex];
-            availableLanes.RemoveAt(randomLaneIndex);
-            Vector3 spawnPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, transform.position.z);
+            if (availableLanes.Count <= 0) break; // If there are no more available lanes, break the loop
+            int selectedLane = SelectLane();
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
             Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);        
         }
         
-        
+    }
+
+    void SpawnApple()
+    {
+        if(Random.value > appleSpawnChance || availableLanes.Count <= 0) return; // If the random value is greater than the apple spawn chance, do not spawn an apple
+
+        int selectedLane = SelectLane();
+        Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+        Instantiate(applePrefab, spawnPosition, Quaternion.identity, this.transform); 
+    }
+
+    void SpawnCoins()
+    {
+        if (Random.value > coinSpawnChance || availableLanes.Count <= 0) return; // If the random value is greater than the coin spawn chance, do not spawn coins
+
+        int selectedLane = SelectLane();
+        int maxCoinsToSpawn = 6;
+        int coinsToSpawn = Random.Range(1, maxCoinsToSpawn );
+        float topOfChunkZPos = transform.position.z + (coinSeparationLength * 2f);
+
+        for (int i = 0; i < coinsToSpawn; i++)
+        {
+            float spawnPositionZ = topOfChunkZPos - (coinSeparationLength * i); // Increment Z position for each coin
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, spawnPositionZ);
+            Instantiate(coinPrefab, spawnPosition, Quaternion.identity, this.transform); 
+        }
+
+    }
+
+    int SelectLane()
+    {
+        int randomLaneIndex = Random.Range(0, availableLanes.Count);
+        int selectedLane = availableLanes[randomLaneIndex];
+        availableLanes.RemoveAt(randomLaneIndex); // Remove the lane to prevent spawning in the same lane again
+        return selectedLane; 
     }
 }
