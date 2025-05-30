38d3ffb5323da8226ce3a7270d307bbe0fcbc9a3
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2025-05-30 14:36:56 +0100

    Update Level Generator
    Floor is now instantiate one at a time with its .z position moved 10f to the next space

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -3,10 +3,30 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] GameObject chunkPrefab;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] int startingChunksAmount = 12;
+    [SerializeField] Transform chunkParent;
+
+    [SerializeField] float chunkLength = 10f; // Length of each chunk, serialized for adjustment if prefabs is bigger or smaller
+
     void Start()
     {
-      Instantiate(chunkPrefab, transform.position, Quaternion.identity);  
+        for(int i = 0; i < startingChunksAmount; i++)
+        {
+            float spawnPositionZ;
+
+            if (i == 0)
+            {
+                spawnPositionZ = transform.position.z;
+            }
+            else
+            {
+                spawnPositionZ = transform.position.z + (i * chunkLength); // Each subsequent chunk is placed after the previous one
+            
+            }
+
+            Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+            Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent);  
+        }
     }
 
 }
