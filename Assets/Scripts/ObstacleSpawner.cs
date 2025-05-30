a448995aa75fb3b07a870faed20f2b2f821ec489
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2025-05-30 19:04:37 +0100

    Add objects for pooling
    More objects added with unique collliders

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -6,6 +6,7 @@ public class ObstacleSpawner : MonoBehaviour
     [SerializeField] GameObject[] obstaclePrefabs; // Prefab of the obstacle to spawn
     [SerializeField] float obstacleSpawnTime = 1f; // Time interval between spawns
     [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth = 4f; // Width of the spawn area
 
 
     void Start()
@@ -19,9 +20,10 @@ public class ObstacleSpawner : MonoBehaviour
     {
         while (true)
         {      
-            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];      
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)]; 
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z);     
             yield return new WaitForSeconds(obstacleSpawnTime);
-            Instantiate(obstaclePrefab, transform.position, Random.rotation, obstacleParent);
+            Instantiate(obstaclePrefab, spawnPosition, Random.rotation, obstacleParent);
 
         }
 
