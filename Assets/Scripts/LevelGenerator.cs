7b4e2776df955041b97c247890763c31970b1429
Sergi Ramon <bi23tl@student.sunderland.ac.uk>
2025-05-30 15:01:13 +0100

    Add array to store chunks clones
    The chunks are stored in an array used to instantiate each chunk

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,28 +5,40 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] GameObject chunkPrefab;
     [SerializeField] int startingChunksAmount = 12;
     [SerializeField] Transform chunkParent;
-
     [SerializeField] float chunkLength = 10f; // Length of each chunk, serialized for adjustment if prefabs is bigger or smaller
+
+    GameObject[] chunks = new GameObject[12];
 
     void Start()
     {
-        for(int i = 0; i < startingChunksAmount; i++)
-        {
-            float spawnPositionZ;
-
-            if (i == 0)
-            {
-                spawnPositionZ = transform.position.z;
-            }
-            else
-            {
-                spawnPositionZ = transform.position.z + (i * chunkLength); // Each subsequent chunk is placed after the previous one
-            
-            }
-
-            Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
-            Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent);  
-        }
+        SpawnChunks();
     }
 
+    private void SpawnChunks()
+    {
+        for (int i = 0; i < startingChunksAmount; i++)
+        {
+            float spawnPositionZ = CalculateSpawnPositionZ(i);
+            Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+            GameObject newChunk = Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent);
+
+            chunks[i] = newChunk;
+        }
+    
+    }
+
+    private float CalculateSpawnPositionZ(int i)
+    {
+        float spawnPositionZ;
+
+        if(i == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            spawnPositionZ = transform.position.z + (i* chunkLength);
+        }
+        return spawnPositionZ;
+    }
 }
